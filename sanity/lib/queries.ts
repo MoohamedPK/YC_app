34c397c369cf9b3cc717eb3568cabb6982f5e7a6
Mohamed <mohamed3atwani2004@gmail.com>
2025-04-14 21:47:23 +0100

    feat: author athentication

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -3,4 +3,20 @@ import { defineQuery } from "next-sanity";
 export const STARTUP_QEURIES = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
     
     _id,title,slug,_createdAt, author -> {_id, name, image, bio}, views, description, category, image
+<<<<<<< Updated upstream
+=======
+    }`)
+
+export const STARTUP_BY_ID_QEURIES = defineQuery(`*[_type == "startup" && _id == $id ][0] {
+    
+    _id,title,slug,_createdAt, author -> {_id, name, username, image, bio}, views, description, category, image, pitch
+    }`)
+
+export const STARTUP_VIEWS = defineQuery(`*[_type == "startup" && _id == $id ][0] {
+        _id, views
+    }`)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == "author" && _id == $id ][0] {
+    _id,id,name,username,image,bio,email
+>>>>>>> Stashed changes
     }`)
